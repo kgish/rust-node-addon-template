0b7e9c49295e2cee359ba59e6aa1dfc29a5e41a5
Kiffin Gish <kiffin.gish@planet.nl>
2020-05-08 12:26:55 +0200

    send_message function

--- index.js
+++ index.js
@@ -12,11 +12,17 @@ if (n === 0 || n === 1) {
 
 if (n === 0 || n === 2) {
     console.log(`index.js: addon.say_hello = '${addon.say_hello}'`);
-    console.log(`index.js: addon.say_hello() = '${addon.say_hello()}'`);
+    console.log(`index.js: addon.say_hello() => '${addon.say_hello()}'`);
 }
 
 if (n === 0 || n === 3) {
     console.log(`index.js: addon.add_doubles = '${addon.add_doubles}'`);
-    console.log(`index.js: addon.add_doubles(3.1,4.3) = '${addon.add_doubles(3.1, 4.3)}'`);
+    console.log(`index.js: addon.add_doubles(3.1,4.3) => '${addon.add_doubles(3.1, 4.3)}'`);
+}
+
+if (n === 0 || n === 4) {
+    const message = 'This is a message from the javascript world!';
+    console.log(`index.js: addon.send_message = '${addon.send_message}'`);
+    console.log(`index.js: addon.send_message('${message}') => '${addon.send_message(message)}'`);
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,9 @@ use nodejs_sys::{
     napi_create_string_utf8,
     napi_env,
     napi_get_cb_info,
+    napi_get_undefined,
     napi_get_value_double,
+    napi_get_value_string_utf8,
     napi_set_named_property,
     napi_value,
 };
@@ -35,6 +37,8 @@ use std::ffi::CString;
 //   napi_get_cb_info: https://docs.rs/nodejs-sys/0.3.0/nodejs_sys/fn.napi_get_cb_info.html
 //   napi_get_value_double: https://docs.rs/nodejs-sys/0.3.0/nodejs_sys/fn.napi_get_value_double.html
 //   napi_create_double: https://docs.rs/nodejs-sys/0.3.0/nodejs_sys/fn.napi_create_double.html
+//   napi_get_undefined: https://docs.rs/nodejs-sys/0.3.0/nodejs_sys/fn.napi_get_undefined.html
+//   napi_get_value_string_utf8: https://docs.rs/nodejs-sys/0.3.0/nodejs_sys/fn.napi_get_value_string_utf8.html
 
 // --- Register module --- //
 
@@ -82,7 +86,7 @@ pub unsafe extern "C" fn napi_register_module_v1(
 
     napi_set_named_property(env, exports, str_ptr(&str2), result2);
 
-    // --- 3. Create a function passing integers: add_doubles() -> double --- //
+    // --- 3. Create a function passing doubles: add_doubles() -> double --- //
 
     let str3 = CString::new("add_doubles").expect("CString::new failed");
     let mut result3: napi_value = std::mem::zeroed();
@@ -98,7 +102,21 @@ pub unsafe extern "C" fn napi_register_module_v1(
 
     napi_set_named_property(env, exports, str_ptr(&str3), result3);
 
-    // --- 4. Create a function passing strings--- //
+    // --- 4. Create a function passing string: send_message(message) -> () --- //
+
+    let str4 = CString::new("send_message").expect("CString::new failed");
+    let mut result4: napi_value = std::mem::zeroed();
+
+    napi_create_function(
+        env,
+        str_ptr(&str4),
+        str_len(&str4),
+        Some(send_message),
+        std::ptr::null_mut(),
+        &mut result4,
+    );
+
+    napi_set_named_property(env, exports, str_ptr(&str4), result4);
 
     exports
 }
@@ -153,6 +171,55 @@ pub unsafe extern "C" fn add_doubles(env: napi_env, info: napi_callback_info) ->
     napi_create_double(env, value, &mut result);
 
     result
+}
+
+pub unsafe extern "C" fn send_message(env: napi_env, info: napi_callback_info) -> napi_value {
+    // Extract the initialized data -- this is only allowed *after* properly initializing `buffer`
+    let mut message: [napi_value; 1] = std::mem::MaybeUninit::zeroed().assume_init();
+    let mut argc = 1 as usize;
+
+    napi_get_cb_info(
+        env,
+        info,
+        &mut argc,
+        message.as_mut_ptr(),
+        std::ptr::null_mut(),
+        std::ptr::null_mut(),
+    );
+
+    let mut len = 0;
+
+    napi_get_value_string_utf8(
+        env,
+        message[0],
+        std::ptr::null_mut(),
+        0,
+        &mut len
+    );
+
+    let size = len as usize;
+    let mut vec: Vec<u8> = Vec::with_capacity(size + 1);
+    let raw = vec.as_mut_ptr();
+    std::mem::forget(vec);
+    let mut capacity = 0;
+
+    let _s = napi_get_value_string_utf8(
+        env,
+        message[0],
+        raw as *mut i8,
+        size + 1,
+        &mut capacity
+    );
+
+    let s = String::from_raw_parts(raw, capacity as usize, size);
+
+    println!("lib.rs: send_message({})", s);
+
+    // Function
+    let mut und: napi_value = std::mem::zeroed();
+    napi_get_undefined(env, &mut und);
+
+    und
 }
 
 // --- Private --- //
